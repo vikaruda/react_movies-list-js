d6e319281c8466e18db2ee0f53f7cb9c553466d4
Vika Ruda <rudaviktoria0@gmail.com>
2025-01-04 16:33:23 +0200

    Bug1633

--- src/App.jsx
+++ src/App.jsx
@@ -2,53 +2,12 @@
 
 import './App.scss';
 import moviesFromServer from './api/movies.json';
-
-const Card = ({ movies }) => {
-  <div className="card" data-cy="Movie">
-    <div className="card-image">
-      <figure className="image is-4by3">
-        <img data-cy="MovieImage" src={movies.imgUrl} alt="Film logo" />
-      </figure>
-    </div>
-
-    <div className="card-content">
-      <div className="media">
-        <div className="media-left">
-          <figure className="image is-48x48">
-            <img src="images/imdb-logo.jpeg" alt="imdb" />
-          </figure>
-        </div>
-
-        <div className="media-content">
-          <p className="title is-8" data-cy="MovieTitle">
-            Inception
-          </p>
-        </div>
-      </div>
-
-      <div className="content">
-        <p data-cy="MovieDescription">
-          Follows the lives of eight very different couples in dealing with
-          their love lives in various loosely interrelated tales all set during
-          a frantic month before Christmas in London, England.
-        </p>
-
-        <a href="https://www.imdb.com/title/tt1375666" data-cy="MovieLink">
-          IMDB
-        </a>
-      </div>
-    </div>
-  </div>;
-};
+import { MovieList } from './components/MovieList/MovieList';
 
 export const App = () => (
   <div className="page">
     <div className="page-content">
-      <div className="movies">
-        {moviesFromServer.map(movies => (
-          <Card movies={movies} key={movies.imdbId} />
-        ))}
-      </div>
+      <MovieList moviesFromServer={moviesFromServer} />
     </div>
 
     <div className="sidebar" data-cy="Sidebar">
